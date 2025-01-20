9a971c18d1a6b582515797dc37663e54f6715650
Byron Villegas Moya <byronvillegasm@gmail.com>
2025-01-20 04:33:57 -0300

    Changed CI/CD.yml and AcceptanceTestRunner

--- src/acceptance-test/java/acceptance/AcceptanceTestRunner.java
+++ src/acceptance-test/java/acceptance/AcceptanceTestRunner.java
@@ -1,9 +1,11 @@
 package acceptance;
 
 import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(features = "src/acceptance-test/resources/dev")
 public class AcceptanceTestRunner {
 
 }
